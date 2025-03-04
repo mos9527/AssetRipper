b494692243d252ce80a7e00c67722e8f8b603840
mos9527 <greats3an@gmail.com>
2025-03-04 20:05:56 +0800

    Update missing UnityExtension definitions

--- Source/AssetRipper.Export.UnityProjects/Scripts/ReferenceAssemblies.cs
+++ Source/AssetRipper.Export.UnityProjects/Scripts/ReferenceAssemblies.cs
@@ -2,6 +2,7 @@
 
 namespace AssetRipper.Export.UnityProjects.Scripts;
 
+// GUID, Version Min, Version Max (non-inclusive)
 using UnityExtensionAssembly = Tuple<UnityGuid, UnityVersion, UnityVersion>;
 public static partial class ReferenceAssemblies
 {
@@ -46,20 +47,21 @@ public static partial class ReferenceAssemblies
 	];
 
 	private static readonly Dictionary<string, UnityExtensionAssembly> UnityExtensionAssemblies =
-		new Dictionary<string, UnityExtensionAssembly>() {		
+		new Dictionary<string, UnityExtensionAssembly>() {
+			{"UI",new UnityExtensionAssembly(new UnityGuid(0x25C76F5F, 0x4FD4651D, 0xDCC6398A, 0x8DB3A202), new UnityVersion(5), new UnityVersion(2019))},
 			{"Networking",new UnityExtensionAssembly(new UnityGuid(0x98353078, 0x2E043BB1, 0x078C9A2B, 0xAB9147E7), new UnityVersion(5), new UnityVersion(2019))},
-			{"PerformanceTesting",new UnityExtensionAssembly(new UnityGuid(0xBB836ED0, 0x8024F66B, 0xAE06BFBA, 0x423040E8), new UnityVersion(2017), new UnityVersion(2019))},
+			{"PerformanceTesting",new UnityExtensionAssembly(new UnityGuid(0xBB836ED0, 0x8024F66B, 0xAE06BFBA, 0x423040E8), new UnityVersion(2017), new UnityVersion(2018))},
 			{"TestRunner",new UnityExtensionAssembly(new UnityGuid(0xAAFCBE35, 0xD2E4E1E2, 0xC28858CB, 0x1AF37A5D), new UnityVersion(5), new UnityVersion(2019))},
 			{"Timeline",new UnityExtensionAssembly(new UnityGuid(0x092B01A6, 0xF7843829, 0x9D00B319, 0x5FAF3DC4), new UnityVersion(2017), new UnityVersion(2019))},
 			{"UIAutomation",new UnityExtensionAssembly(new UnityGuid(0xCBCA867C, 0x93D42FC9, 0xC4BB8848, 0x88219B39), new UnityVersion(2017), new UnityVersion(2018))},
 			{"GoogleAudioSpatializer",new UnityExtensionAssembly(new UnityGuid(0xB1FC4F4E, 0x731434B9, 0xA309994A, 0xA156D1A7), new UnityVersion(2017), new UnityVersion(2019))},
 			{"HoloLens",new UnityExtensionAssembly(new UnityGuid(0x4FF45B7F, 0xFCF4D34A, 0xB8354B18, 0xCCB0E876), new UnityVersion(5), new UnityVersion(2018))},
 			{"SpatialTracking",new UnityExtensionAssembly(new UnityGuid(0x3F3437DE, 0x3B3483E0, 0xB8F8ADFA, 0xAEC96620), new UnityVersion(2017), new UnityVersion(2019))},
-			{"Advertisements",new UnityExtensionAssembly(new UnityGuid(0xF9DBB937, 0x8624B463, 0x8DF9F478, 0xFEEB3BA6), new UnityVersion(5), new UnityVersion(2017))},
-			{"Analytics",new UnityExtensionAssembly(new UnityGuid(0x0865E258, 0x36149BE2, 0x194BBCA8, 0x0B794418), new UnityVersion(5), new UnityVersion(2017))},
-			{"Purchasing",new UnityExtensionAssembly(new UnityGuid(0xDE8DC0E8, 0xC2144D44, 0x602460EB, 0x44E9CBA7), new UnityVersion(5), new UnityVersion(2017))},
-			{"VR",new UnityExtensionAssembly(new UnityGuid(0xC5E1FDC6, 0x02741D87, 0x4DCCDAAA, 0x69FD297C), new UnityVersion(5), new UnityVersion(2017))},
-	};
+			{"Advertisements",new UnityExtensionAssembly(new UnityGuid(0xF9DBB937, 0x8624B463, 0x8DF9F478, 0xFEEB3BA6), new UnityVersion(5), new UnityVersion(6))},
+			{"Analytics",new UnityExtensionAssembly(new UnityGuid(0x0865E258, 0x36149BE2, 0x194BBCA8, 0x0B794418), new UnityVersion(5), new UnityVersion(6))},
+			{"Purchasing",new UnityExtensionAssembly(new UnityGuid(0xDE8DC0E8, 0xC2144D44, 0x602460EB, 0x44E9CBA7), new UnityVersion(5), new UnityVersion(6))},
+			{"VR",new UnityExtensionAssembly(new UnityGuid(0xC5E1FDC6, 0x02741D87, 0x4DCCDAAA, 0x69FD297C), new UnityVersion(5), new UnityVersion(6))},
+		};
 
 	public static UnityGuid GetUnityExtensionAssemblyGuid(string namespaceName, string name)
 	{
